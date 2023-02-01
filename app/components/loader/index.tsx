448094956589f0224fe70114b836ab8292456e24
george-hub331 <idiaghegeorge9@gmail.com>
2023-02-01 20:23:10 +0100

    update: add firebase db

---
+++ app/components/loader/index.tsx
@@ -0,0 +1,43 @@
+import { Box, CircularProgress } from "@mui/material";
+import logo from "../../../public/images/logo.png"
+import Image from "next/image";
+
+const Loader = ({
+  incLogo = true,
+  text = "",
+  fixed = true,
+  sx = {},
+}: {
+  incLogo?: boolean;
+  text?: any;
+  fixed?: boolean;
+  sx?: object;
+}) => {
+  return (
+    <div
+      style={{
+        position: fixed ? "fixed" : "absolute",
+        justifyContent: text ? "space-around" : "center",
+        ...sx,
+      }}
+      className="h-full left-0 bg-white top-0 z-[100] flex flex-col justify-center items-center w-full"
+    >
+      {incLogo && (
+        <div className="items-center flex justify-center">
+          <Image src={logo} alt="Clover" width={150} height={49.995} />
+        </div>
+      )}
+      <Box
+        className="text-[#1890FF] mt-11 justify-center "
+        sx={{ display: "flex" }}
+      >
+        <CircularProgress size={text ? 80 : 60} color="inherit" />
+      </Box>
+      {text !== "" && (
+        <div className="text-[#1890FF] text-xl font-bold">{text}</div>
+      )}
+    </div>
+  );
+};
+
+export default Loader;
---
+++ app/firebase/index.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY || "",
+  authDomain: process.env.FIREBASE_AUTHDOMAIN || "",
+  databaseURL: process.env.FIREBASE_DATABASEURL || "",
+  projectId: process.env.FIREBASE_PROJECTID || "",
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET || "",
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID || "",
+  appId: process.env.FIREBASE_APPID || "",
+  measurementId: process.env.FIREBASE_MEASUREMENTID || "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
