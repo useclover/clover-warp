c1d6adab6ae6b2b3ddbeca2e064ce533ed76b91c
george-hub331 <idiaghegeorge9@gmail.com>
2023-02-06 15:50:22 +0100

    ...

--- app/firebase/index.ts
+++ app/firebase/index.ts
@@ -8,14 +8,19 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY || "",
-  authDomain: process.env.FIREBASE_AUTHDOMAIN || "",
-  databaseURL: process.env.FIREBASE_DATABASEURL || "",
-  projectId: process.env.FIREBASE_PROJECTID || "",
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET || "",
-  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID || "",
-  appId: process.env.FIREBASE_APPID || "",
-  measurementId: process.env.FIREBASE_MEASUREMENTID || "",
+  apiKey:
+    process.env.FIREBASE_APIKEY || "AIzaSyBzJLms6P1gAmiksq9d8gNf44F2arvWoeg",
+  authDomain: process.env.FIREBASE_AUTHDOMAIN || "clover-6aac1.firebaseapp.com",
+  databaseURL:
+    process.env.FIREBASE_DATABASEURL ||
+    "https://clover-6aac1-default-rtdb.firebaseio.com",
+  projectId: process.env.FIREBASE_PROJECTID || "clover-6aac1",
+  storageBucket:
+    process.env.FIREBASE_STORAGEBUCKET || "clover-6aac1.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID || "1036733483114",
+  appId:
+    process.env.FIREBASE_APPID || "1:1036733483114:web:45b3b72229e5c58041e213",
+  measurementId: process.env.FIREBASE_MEASUREMENTID || "G-PFZ9MWN052",
 };
 
 // Initialize Firebase
